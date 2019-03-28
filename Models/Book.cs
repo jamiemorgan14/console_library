5a1908c2d5749f0161394d93a2d319d13ed7f6e8
Jamie <jmorgan3475@gmail.com>
2019-03-27 21:51:01 -0600

    created Book model

---
+++ Models/Book.cs
@@ -0,0 +1,10 @@
+namespace console_library.Models
+{
+  public class Book
+  {
+    //properties
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public bool Available { get; set; }
+  }
+}
